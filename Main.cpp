5c3c0f9c0054584bcab581d1ac1382f91da9a268
ee21b079 <ee21b079@smail.iitm.ac.in>
2024-04-27 21:22:02 +0530

    Added audio processing

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <openacc.h>
 #include <opencv2/core/types.hpp>
-#include <string>
 #ifndef OPENCV
 #define OPENCV
 #include <opencv2/core.hpp>
@@ -24,7 +22,6 @@
 using namespace cv;
 
 void printmatrix(Matrix *&mat) {
-  cout << "going to print matrix" << endl;
   std::cout.precision(3);
   for (int i = 0; i < mat->rows / 50; i++) {
     for (int j = 0; j < mat->cols / 50; j++) {
@@ -35,48 +32,54 @@ void printmatrix(Matrix *&mat) {
   printf("\n\n");
 }
 
-int main(int argc, char *argv[]) {
-  acc_device_t dev_type = acc_get_device_type();
-  if (dev_type == acc_device_none) {
-    printf("No accelerator device is being used.\n");
-  } else if (dev_type == acc_device_host) {
-    printf("The CPU is being used as the accelerator.\n");
-  } else if (dev_type == acc_device_not_host) {
-    printf("An accelerator device other than the CPU is being used.\n");
-  } else if (dev_type == acc_device_nvidia) {
-    printf("An NVIDIA GPU is being used as the accelerator.\n");
-  } else {
-    printf("Unknown accelerator device is being used.\n");
-  }
+void generate_mat(Matrix *&ret) {
+  unsigned int rows = 32;
+  unsigned int cols = 64;
+  ret->rows = rows;
+  ret->cols = cols;
+  cout << "gonna fail" << endl;
+  ret->matrix = new std::complex<double>[rows * cols];
+  for (int i = 0; i < rows; i++)
+    for (int j = 0; j < cols; j++) {
+      ret->matrix[i * cols + j].real(rand() % 255);
+      ret->matrix[i * cols + j].imag(0);
+    }
+}
 
+int main(int argc, char *argv[]) {
   printf("Hello\n");
   if (argc == 1) {
-    printf("Given image path as first argument\n");
+    printf("Given file path as first argument\n");
     exit(0);
   }
-  std::string img_path(argv[1]);
-  std::string window_name = "Original image";
-  cv::Mat img = imread(img_path, IMREAD_GRAYSCALE);
+  cout << "Chosse 1 for images and 2 for audio" << endl;
+  int choice;
+  cin >> choice;
+  if (choice == 1) {
+    std::string img_path(argv[1]);
+    std::string window_name = "Original image";
+    Mat img = imread(img_path, IMREAD_GRAYSCALE);
 
-  if (img.empty()) {
-    printf("Couldnt read image\n");
-    return 1;
+    if (img.empty()) {
+      printf("Couldnt read image\n");
+      return 1;
+    }
+
+    Matrix *img_matrix = cv_to_std(img);
+    cout << img_matrix->cols << " is new cols" << endl;
+    cout << img_matrix->rows << " is new rows" << endl;
+    Matrix *fft_matrix = fft2d(img_matrix, 0);
+    gauss(fft_matrix, fft_matrix->rows, 0.05);
+    Matrix *new_mat = fft2d(fft_matrix, 1);
+    cv::Mat new_cv = std_to_cv(new_mat);
+    imshow(window_name, new_cv);
+    waitKey(0);
+    printf("finished showing image\n");
+  } else if (choice == 2) {
+    cout << "Not done yet" << endl;
+    exit(0);
+  } else {
+    cout << "Invalid" << endl;
+    exit(1);
   }
-
-  namedWindow(window_name, WINDOW_NORMAL);
-  Matrix *img_matrix = cv_to_std(img);
-  // cv::imshow(window_name, img);
-  // waitKey(0);
-  cout << img_matrix->cols << " is new cols" << endl;
-  cout << img_matrix->rows << " is new rows" << endl;
-  Matrix *fft_matrix = fft2d(img_matrix, 0);
-  double cut_off = stof(argv[2]);
-  cout << cut_off << endl;
-  // gauss(fft_matrix, fft_matrix->rows, fft_matrix->rows);
-  low_pass(fft_matrix, cut_off);
-  Matrix *new_mat = fft2d(fft_matrix, 1);
-  cv::Mat new_cv = std_to_cv(new_mat);
-  cv::imshow(window_name, new_cv);
-  waitKey(0);
-  printf("finished showing image\n");
 }
